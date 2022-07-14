d8db9cf905a50e8973daf3e54ec8e22c301aa9fd
Arthur F <44340520+arthurforgac@users.noreply.github.com>
2022-07-15 00:39:59 +0200

    Update settings.py

--- microservice/settings.py
+++ microservice/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-79e03^d24u%9)_+u*+$m=b-e28=1h#$^l6sg778lque&$xfz2@'
+SECRET_KEY = 'SOME-SECRET-KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
